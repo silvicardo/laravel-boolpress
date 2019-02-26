c7cb53f06f3159a476de34ffe49de26e888ea9d3
Riccardo Silvi <silvicardo@gmail.com>
2019-02-26 16:54:42 +0100

    admin/homeController and public homeController with routes, home pages

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
 Auth::routes();
+
+Route::get('/admin', 'Admin\HomeController@index')->name('admin.home')->middleware('auth');
 
 Route::get('/home', 'HomeController@index')->name('home');
