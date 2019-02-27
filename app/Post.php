dfeee714445c6f7ff52f6715e3467d5813cb38e7
Riccardo Silvi <silvicardo@gmail.com>
2019-02-27 11:05:32 +0100

    post - category models relationship

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['category_id','title', 'author', 'content'];
+
+    public function category()
+
+    {
+      return $this->belongsTo('App\Category');
+    }
 }
